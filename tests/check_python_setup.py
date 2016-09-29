ff5c45ce6c9557368d350e895ebf216897788094
Simeon Warner <simeon.warner@cornell.edu>
2016-09-29 09:42:34 -0400

    Infrastructure

---
+++ tests/check_python_setup.py
@@ -0,0 +1,17 @@
+"""Check setup of Python for this tutorial."""
+
+import sys
+import unittest
+
+class TestCheckPythonSetup(unittest.TestCase):
+
+    def test_python_version(self):
+        """Need >=2.7 or >=3.3."""
+        v =  sys.version_info
+        if (v[0] == 2):
+            self.assertTrue(v >= (2, 7), "Python2 >= 2.7")
+        else:
+            self.assertTrue(v >= (3, 3), "Python3 >= 3.3")
+
+if __name__ == '__main__':
+    unittest.main()
