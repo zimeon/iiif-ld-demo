4e4eecca25168b0db55c2b44b06f48460e16064b
Simeon Warner <simeon.warner@cornell.edu>
2016-11-23 23:31:28 -0500

    Reduce duplication

--- context_cache/rdflib_jsonld.py
+++ context_cache/rdflib_jsonld.py
@@ -9,27 +9,20 @@ from .local_cache import in_cache
 if PY3:
     from io import StringIO
 
+# Store reference to orginal function so we can wrap it
+original_source_to_json = util.source_to_json
+
 
 def source_to_json_with_cache(source):
     """Look for local cached version of source, else fallback to loading."""
-    # Based on rdflib.util.source_to_json
     filepath = in_cache(source)
     if (filepath is None):
         logging.debug("Using default loader to get %s" % (source))
-        source = create_input_source(source, format='json-ld')
     else:
         logging.debug("Reading %s from %s" % (source, filepath))
-        source = create_input_source(filepath, format='json-ld')
-
-    stream = source.getByteStream()
-    try:
-        if PY3:
-            return json.load(StringIO(stream.read().decode('utf-8')))
-        else:
-            return json.load(stream)
-    finally:
-        stream.close()
+        source = filepath
+    return original_source_to_json(source)
 
 
-# On load set up rdflib code to use cached loader
+# On load set up rdflib code to use loader with cache
 util.source_to_json = source_to_json_with_cache
